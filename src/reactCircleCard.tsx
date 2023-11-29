da9c345d327abc484e437e367ef909665faa882d
eduardo <eduardoperezmu@gmail.com>
2023-11-29 11:40:36 -0600

    me lleva la v

--- src/reactCircleCard.tsx
+++ src/reactCircleCard.tsx
@@ -1,13 +1,27 @@
 import * as React from "react";
 
-export class ReactCircleCard extends React.Component<{}>{
-    render(){
+interface State {
+    base64Image: string | null;
+    loading: boolean;
+    error: Error | null;
+}
+
+class ReactCircleCard extends React.Component<{}, State> {
+    constructor(props: {}) {
+        super(props);
+    }
+    componentDidMount() {
+
+    }
+
+    render() {
+
         return (
             <div className="circleCard">
-                Hello, Efrain
-                <img src="https://app.cuidadoconelperro.com.mx/media/catalog/product/1/_/1_42377.jpg"></img>
+                Aqui debe de ir la imagen
+                 <img src="https://app.cuidadoconelperro.com.mx/media/catalog/product/1/_/1_42377.jpg" alt="Descripción de la imagen" />
             </div>
-        )
+        );
     }
 }
 
