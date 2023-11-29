04764cb4d88ca0080845af685b4c210edc96aa25
biecom <biecom@agarcia.com.mx>
2023-11-28 18:32:58 -0600

    first commit

---
+++ src/reactCircleCard.tsx
@@ -0,0 +1,14 @@
+import * as React from "react";
+
+export class ReactCircleCard extends React.Component<{}>{
+    render(){
+        return (
+            <div className="circleCard">
+                Hello, Efrain
+                <img src="https://app.cuidadoconelperro.com.mx/media/catalog/product/1/_/1_42377.jpg"></img>
+            </div>
+        )
+    }
+}
+
+export default ReactCircleCard;
