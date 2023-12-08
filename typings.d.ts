9cfd8ccb4c98a44ea9db69ecced56e2fa20c582e
eduardo <eduardoperezmu@gmail.com>
2023-12-08 11:23:59 -0600

    update api 2.4

---
+++ typings.d.ts
@@ -0,0 +1,7 @@
+declare namespace powerbi {
+    export interface VisualObjectInstance {
+        // Define tus interfaces según tus necesidades
+    }
+
+    // Agrega más interfaces según sea necesario
+}
