856c316b94970233c8a74c7bad167118f425b585
eduardo <eduardoperezmu@gmail.com>
2023-12-12 12:17:53 -0600

    fix error image

--- src/components/convertImageABase64.tsx
+++ src/components/convertImageABase64.tsx
@@ -1,25 +1,24 @@
 import * as React from "react";
 
-const convertImageABase64 = async (image: string): Promise<string> => {
-  
-  try {
-    const response = await fetch(image);
-
-    if (!response.ok) {
-      throw new Error(`Failed to fetch image. Status: ${response.status}`);
-    }
-
-    const blob = await response.blob();
-    return new Promise((resolve, reject) => {
+const blobToBase64 = (blob: Blob): Promise<string> => {
+  return new Promise((resolve, reject) => {
       const reader = new FileReader();
       reader.onloadend = () => resolve(reader.result as string);
       reader.onerror = reject;
       reader.readAsDataURL(blob);
-    });
-  } catch (error) {
-    console.error("Error al convertir la imagen a base64:", error);
-    return ""; // Retornar una cadena vacía en caso de error
-  }
+  });
+};
+
+const convertImageABase64 = async (imageUrl: string): Promise<string | null> => {
+ 
+    console.log('esto mando', imageUrl)
+    const response = await fetch(imageUrl);
+    const blob = await response.blob();
+    const base64 = await blobToBase64(blob);
+    (console.log('yaaaa',base64))
+    return base64;
+
+ 
 };
 
 export default convertImageABase64;
--- src/components/img.tsx
+++ src/components/img.tsx
@@ -8,19 +8,11 @@ interface imgProps {
 
 const ImageStyle: React.FC<imgProps> = ({ image }) => {
     const [convertedImage, setConvertedImage] = React.useState<string | null>(null);
-    console.log("aqui deberia llegar la imagen",image)
+   
     React.useEffect(() => {
         const fetchAndConvertImage = async () => {
-            try {
-                if (image) {
-                    
                     const result = await convertImageABase64(image);
                     setConvertedImage(result);
-                    console.log('aqui debe regresar convertido', result)
-                }
-            } catch (error) {
-                console.error("Error al obtener la imagen:", error);
-            }
         };
         fetchAndConvertImage();
     }, [image]);
